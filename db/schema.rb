002855944a7192f630e40d12e8483f1429f48937
kalunge <muthomititus+github@gmail.com>
2022-03-09 14:33:23 +0100

    Update schema to include code review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_131923) do
+ActiveRecord::Schema.define(version: 2022_03_09_133207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "code_reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "pull_request_link"
+    t.string "description"
+    t.boolean "reviewed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
